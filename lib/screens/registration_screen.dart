81571e2db9c6e18bf304ca0e08a651a30e5faf37
Michael <zhangzunke@126.com>
2025-07-02 21:30:37 +0800

    add text animation and refactor code

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flash_chat_flutter/components/rounded_button.dart';
+import 'package:flash_chat_flutter/constants.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -35,75 +37,24 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             Padding(
               padding: EdgeInsetsGeometry.symmetric(vertical: 12.0),
               child: TextField(
-                decoration: InputDecoration(
+                decoration: kTextFieldDecoration.copyWith(
                   hintText: 'Enter your email',
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 20.0,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.lightBlueAccent,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.lightBlueAccent,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
                 ),
               ),
             ),
             Padding(
               padding: EdgeInsetsGeometry.symmetric(vertical: 12.0),
               child: TextField(
-                decoration: InputDecoration(
+                decoration: kTextFieldDecoration.copyWith(
                   hintText: 'Enter your password',
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 20.0,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.lightBlueAccent,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.lightBlueAccent,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
                 ),
               ),
             ),
             SizedBox(height: 24.0),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {},
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text('Register'),
-                ),
-              ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              title: 'Register',
+              onPressed: () {},
             ),
           ],
         ),
